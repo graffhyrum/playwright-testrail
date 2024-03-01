7a802fc47f5bdb61672691d5857e3fdc94f91cf5
Josh Pendragon <graffhyrum@gmail.com>
2024-02-29 20:11:00 -0600

    swap scripts to pnpm

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,21 +1,15 @@
-import {
-  Reporter,
-  Suite,
-  FullConfig,
-  TestCase,
-  TestResult,
-} from '@playwright/test/reporter';
+import { Reporter, Suite, FullConfig, TestCase, TestResult } from '@playwright/test/reporter';
 
 declare class TestRailReporter implements Reporter {
-  suite: Suite | undefined;
-  private functionQue;
-  private testRailPluginPromise;
-  private testRailPlugin;
-  private testCases;
-  onBegin(config: FullConfig, suite: Suite): Promise<void>;
-  onTestEnd(test: TestCase, result: TestResult): Promise<void>;
-  onEnd(): Promise<void>;
-  onExit(): Promise<void>;
+    suite: Suite | undefined;
+    private functionQue;
+    private testRailPluginPromise;
+    private testRailPlugin;
+    private testCases;
+    onBegin(config: FullConfig, suite: Suite): Promise<void>;
+    onTestEnd(test: TestCase, result: TestResult): Promise<void>;
+    onEnd(): Promise<void>;
+    onExit(): Promise<void>;
 }
 
-export {TestRailReporter as default};
+export { TestRailReporter as default };
